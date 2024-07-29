dad1a32495a49c1894bb0e186c4b5812d8f4de13
Nishaanthini <141409805+Nishaanthini@users.noreply.github.com>
2024-07-29 17:12:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const textarea = document.getElementById("txt");
+const totalCounter = document.getElementById("total");
+const remainingCounter = document.getElementById("remaining");
+textarea.addEventListener("keyup", () => {
+  updateCounter();
+});
+updateCounter()
+function updateCounter() {
+  totalCounter.innerText = textarea.value.length;
+  remainingCounter.innerText =
+    textarea.getAttribute("maxLength") - textarea.value.length;
+}
